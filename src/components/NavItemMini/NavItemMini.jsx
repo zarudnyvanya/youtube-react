bfd187ef6d74c76dc2ea6ec6581501945f386742
quro4ka <quro4ka@gmail.com>
2022-11-10 23:16:56 +0300

    change miniNavigation panel and genre items

--- src/components/NavItemMini/NavItemMini.jsx
+++ src/components/NavItemMini/NavItemMini.jsx
@@ -1,14 +1,11 @@
-import s from './NavItem.module.scss'
+import s from './NavItemMini.module.scss'
 
-export const NavItem = ({ item }) => {
+export const NavItemMini = ({ item }) => {
   return (
     <li className={s.nav__item}>
       <div className={s.nav__icons}>
         <img src={item.image} alt="img" />
       </div>
-      <a href="#" className={s.nav__links}>
-        {item.title}
-      </a>
     </li>
   )
 }
