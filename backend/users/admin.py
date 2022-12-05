0d37fba5672d786c1fd2db0a7a9602c592046159
Daniil <fyfik105@gmail.com>
2022-12-05 22:06:03 +0300

    UserAdmin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import CustomUser
 
+class AdminUser(admin.ModelAdmin):
+    list_display = ('id','email', 'last_name', 'first_name', 'is_staff', 'is_active', 'is_block')
 # Register your models here.
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, AdminUser)
