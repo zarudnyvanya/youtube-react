96a8d7d9b7ca53acceed520ca5f8afb4272bc6b8
quro4ka <quro4ka@gmail.com>
2022-12-10 21:08:40 +0300

    Get user token

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bf5g9=54fej+=3^rh0&eany+jpx-l&4svihm+br9emx57!jx!z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1'
