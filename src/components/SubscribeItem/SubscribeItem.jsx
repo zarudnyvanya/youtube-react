6ea3c84beb7f82c6a770541a506dee0be08cd4e7
ladiick <polevskux@gmail.com>
2022-12-21 01:39:12 +0300

    add eyeShowHide password, create defaultLogo

--- src/components/SubscribeItem/SubscribeItem.jsx
+++ src/components/SubscribeItem/SubscribeItem.jsx
@@ -1,11 +1,11 @@
 
 import s from './SubscribeItem.module.scss'
-
+import userLogo from '../../assets/svg__header/user.png'
 const SubscribeItem = ( {image,nameChanel} )=>{
 
     return(
             <li className={s.subscription__item}>
-                <img src={image} alt=' '/>
+                <img src={image ? image : userLogo } alt=' '/>
                 <p title={nameChanel}>{nameChanel}</p>
             </li>
     )
