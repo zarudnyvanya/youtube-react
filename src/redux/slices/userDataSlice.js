23e495f346f4a18a5d0b17d720221b85f23564e0
quro4ka <quro4ka@gmail.com>
2022-12-23 15:45:19 +0300

    Add videos userChannel

--- src/redux/slices/userDataSlice.js
+++ src/redux/slices/userDataSlice.js
@@ -7,6 +7,41 @@ const initialState = {
     first_name: '',
     lastName: '',
   },
+  userChannel: {
+    pk: null,
+    user: 0,
+    name: '',
+    description: '',
+    image: null,
+    banner: null,
+    logo: null,
+    is_active: true,
+    subscribers: 0,
+  },
+  userVideos: [
+    {
+      id: 17,
+      title: '',
+      description: '',
+      image: '/media/image/2022/12/20/OYbYBiq6bfY_cSU6Fv2.jpg',
+      file: '/media/video/2022/12/20/_React_Pizza_v2__%D0%94%D0%BB%D1%8F_%D0%BA%D0%BE%D0%B3%D0%BE_%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D0%B9_%D0%BA%D1%83%D1%80%D1%81_%D0%B8_%D0%BA%D0%B0%D0%BA_%D0%B5%D0%B3%D0%BE_%D1%81%D0%BC%D0%BE%D1%82%D1%80%D0%B5%D1%82%D1%8C__%D0%A7%D1%82%D0%BE_%D0%BD%D0%BE%D0%B2%D0%BE%D0%B3%D0%BE_.mp4',
+      created_at: '2022-12-20T07:04:12.848982Z',
+      owner: {
+        pk: 9,
+        user: 9,
+        name: 'AdminChannel',
+        description: 'Владу надо отсосать. x2',
+        image: null,
+        banner: null,
+        logo: null,
+        is_active: true,
+        subscribers: 1,
+      },
+      category: [2],
+      views: 0,
+      likes: 1,
+    },
+  ],
   userToken: '',
   isAuth: false,
 }
@@ -18,6 +53,12 @@ const userSlice = createSlice({
     setUserData(state, action) {
       state.userData = action.payload
     },
+    setUserChannel(state, action) {
+      state.userChannel = action.payload
+    },
+    setUserVideos(state, action) {
+      state.userVideos = action.payload
+    },
     setUserToken(state, action) {
       state.userToken = action.payload
     },
@@ -27,6 +68,7 @@ const userSlice = createSlice({
   },
 })
 
-export const { setUserData, setUserToken, setIsAuth } = userSlice.actions
+export const { setUserData, setUserChannel, setUserVideos, setUserToken, setIsAuth } =
+  userSlice.actions
 
 export default userSlice.reducer
