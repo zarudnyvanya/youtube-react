52188247f82cc365ae698151eff8baaf8bae6f3a
Daniil <fyfik105@gmail.com>
2022-12-25 16:33:57 +0300

    api/v1/channel/<pk:int>/mail/

--- backend/mailing/signals.py
+++ backend/mailing/signals.py
@@ -1,16 +1,34 @@
-from django.db.models.signals import post_save
+import os
+from email.mime.image import MIMEImage
+
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from social_core.utils import build_absolute_uri
 
 from mailing.models import ChannelMailingList
 from mailing.services import sendMail
-from video_hosting.models import Video, Channel
-
+from video_hosting.models import Video, Channel, Subscribers
 
 
 @receiver(post_save, sender=Video)
-def save_user_channel(sender, instance, **kwargs):
-	print("Видео создано. я из email")
+def save_user_channel(sender, instance,created, **kwargs):
+	if created:
+		print(instance.image, instance.image.url)
+		channel = instance.channel
+		mylist = ChannelMailingList.objects.filter(channel=channel)
+		email = [elem.user.email for elem in mylist]
+		text_content = 'This is an important message.'
+		html_content = f"<div>Канал {instance.channel.name} Выложил видео {instance.title} в {instance.created_at}</div> <a href='http://127.0.0.1:3000/video/{instance.pk}'>ссылка</a>"
+
+
+		sendMail(text_content=text_content,
+			 	html_content=html_content,
+			 	to=email)
+
+@receiver(post_delete, sender=Subscribers)
+def delete_from_email(sender, instance, **kwargs):
+	user = instance.user
 	channel = instance.channel
-	mylist = ChannelMailingList.objects.filter(channel=channel)
-	email = [elem.user.email for elem in mylist]
-	sendMail(to=email)
+	obj = ChannelMailingList.objects.filter(user=user, channel=channel).limit(1)
+	if obj:
+		obj[0].delete()
--- backend/mailing/urls.py
+++ backend/mailing/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
-from mailing.views import send_mail_view
-
+from mailing.views import MailViewSet
+router = routers.DefaultRouter()
+router.register(r'channel', MailViewSet, basename="mail")
 urlpatterns = [
-    path('send_mail/',  send_mail_view),
+    path('api/v1/', include(router.urls)),
 
 ]
