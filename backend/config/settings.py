52a72a709843fe60294b922930510481f1198e75
Daniil <fyfik105@gmail.com>
2023-01-07 16:52:43 +0300

    some

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -54,11 +54,11 @@ INSTALLED_APPS = [
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/',
-    'http://127.0.0.1:8000/',
-    'http://139.59.147.181/',
-    'http://139.59.147.181:80/',
-    'http://139.59.147.181:432/'
+    'http://localhost:3000',
+    'http://127.0.0.1:8000',
+    'http://139.59.147.181',
+    'http://139.59.147.181:80',
+    'http://139.59.147.181:432'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
