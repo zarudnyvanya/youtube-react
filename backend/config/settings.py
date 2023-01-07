cb827fd59543f168b53f72a3078fd17180d5d5fd
Daniil <fyfik105@gmail.com>
2023-01-07 18:51:16 +0300

    fff

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -74,9 +74,9 @@ CORS_ALLOW_CREDENTIALS = True
 # ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
