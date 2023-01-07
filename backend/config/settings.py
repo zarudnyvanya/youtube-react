cad3e443a2233088218ac655009bbcb7e207a208
Daniil <fyfik105@gmail.com>
2023-01-07 16:50:13 +0300

    test

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -56,7 +56,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000/',
     'http://127.0.0.1:8000/',
-    'http://139.59.147.181',
+    'http://139.59.147.181/',
     'http://139.59.147.181:80/',
     'http://139.59.147.181:432/'
 ]
