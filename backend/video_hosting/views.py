9c1e46be76f37d98a4091b6d68ab85c0233df3cd
Daniil <fyfik105@gmail.com>
2023-01-10 00:03:26 +0300

    fix previous commit

--- backend/video_hosting/views.py
+++ backend/video_hosting/views.py
@@ -3,6 +3,7 @@ from django.http import StreamingHttpResponse
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 
